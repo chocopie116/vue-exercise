751c808f948214ab0a097ee93bc85bc10e6af24d
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-04-16 00:32:32 +0900

    routerがcurrentPageをswitchしてページを表示・非表示切り替えるようにした

--- script/app.js
+++ script/app.js
@@ -8,19 +8,23 @@ var vm = new Vue({
     el: '#js-demo-app',
     data: {
         message: 'Hi. This is Vue.js SPA demo',
-    },
+        currentPage: 'welcome',
+    }
 });
 
 router.addRoute('welcome', function() {
     console.log('here comes welcome');
+    vm.$data.currentPage = 'welcome';
 });
 
 router.addRoute('home', function() {
     console.log('here comes home');
+    vm.$data.currentPage = 'home';
 });
 
 router.addRoute('affiliate/{id}', function(id) {
     console.log('here comes affiliate detail', 'id=' + id);
+    vm.$data.currentPage = 'affiliate';
 });
 
 
