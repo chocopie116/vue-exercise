2e09ca47db4704fc561e3f5512111f4f912f4cd7
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-04-15 11:45:29 +0900

    webpackでcompileしたjsを読み込むようにした

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 var Vue = require('vue');
-
+var vm = new Vue({
+    el: '#myapp',
+    data: {
+        name: 'chocopie116',
+    isAdmin: true,
+    phone: 'iPhone',
+    city: 'tokyo'
+    },
+});
 
