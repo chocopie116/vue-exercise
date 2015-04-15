571e8c93d2b682c4408643a72fa128a92b1b748b
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-04-16 01:33:12 +0900

    案件を表示するようにした

--- script/app.js
+++ script/app.js
@@ -3,12 +3,14 @@ var Vue = require('vue'),
     router = require('crossroads'),
     Listner = require('./listner');
 
+Vue.config.debug = true;
 
 var vm = new Vue({
     el: '#js-demo-app',
     data: {
         message: 'Hi. This is Vue.js SPA demo',
         currentPage: 'welcome',
+        affiliates: []
     }
 });
 
@@ -20,6 +22,11 @@ router.addRoute('welcome', function() {
 router.addRoute('home', function() {
     console.log('here comes home');
     vm.$data.currentPage = 'home';
+    vm.$data.affiliates = [
+        {name: 'aff_1', link: 'https://example.com?id=1'},
+        {name: 'aff_2', link: 'https://example.com?id=2'},
+        {name: 'aff_3', link: 'https://example.com?id=3'},
+    ];
 });
 
 router.addRoute('affiliate/{id}', function(id) {
