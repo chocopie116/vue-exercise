34dfd711608ae2431505c66dc850430c7ce27328
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-04-15 22:21:47 +0900

    routingを実装した

--- script/app.js
+++ script/app.js
@@ -1,8 +1,7 @@
+'use strict';
 var Vue = require('vue'),
-    crossroads = require('crossroads');
+    router = require('crossroads');
 
-console.log(crossroads);
-var Vue = require('vue');
 var vm = new Vue({
     el: '#myapp',
     data: {
@@ -13,5 +12,23 @@ var vm = new Vue({
     },
 });
 
+router.addRoute('welcome', function() {
+    console.log('here comes welcome');
+});
+
+router.addRoute('home', function() {
+    console.log('here comes home');
+});
+
+router.addRoute('affiliate/{id}', function(id) {
+    console.log('here comes affiliate detail', 'id=' + id);
+});
 
 
+var a = document.querySelectorAll('a');
+for (var i=0; i<a.length; i++) {
+    a[i].onclick=function(e){
+        var path = this.href.split('#')[1];
+        router.parse(path);
+    }
+}
