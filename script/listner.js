913a51d7b96f04fb891226f42c501316dc9fdce4
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-04-16 02:06:53 +0900

    初回起動時にもURLをパースするようにした(パーマリンクの作成)

--- script/listner.js
+++ script/listner.js
@@ -6,12 +6,21 @@ var Listner = function(router) {
 
 Listner.prototype.listhenClick = function(targets) {
     var that = this;
+    that._init();
+
     for (var i = 0; i < targets.length; i++) {
         targets[i].onclick = function(e) {
             var path = this.href.split('#')[1];
             that.router.parse(path);
         }
     }
-};
+}
+
+Listner.prototype._init = function() {
+    var that = this;
+    var path = location.href.split('#')[1];
+    that.router.parse(path);
+}
+
 
 module.exports = Listner;
